7feba2106c06de852ad0e979e0b3b87751c559d1
Risa15 <nastyaha2008@bk.ru>
2023-08-08 15:48:48 +0700

    nnn

--- advertisment/app_advertisment/admin.py
+++ advertisment/app_advertisment/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Advertisement
 
 # Register your models here.
+class AdvertisementAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'description', 'prise', 'created_date', 'auction']
+    list_filter = ['auction', 'created_at']
+    actions = ['make_auction_as_false', 'make_auction_as_true']
+
+    @admin.action(description='добавить возможность торга')
+    def make_auction_as_true(self, request, queryset):
+        queryset.updete(auction=True)
+
+    @admin.action(description='убрать возможность торга')
+    def make_auction_as_false(self, request, queryset):
+        queryset.updete(auction=False)
+
+admin.site.register(Advertisement, AdvertisementAdmin)
--- advertisment/app_advertisment/models.py
+++ advertisment/app_advertisment/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib import admin
+from django.utils.html import format_html
+from django.utils import timezone
 
-# Create your models here.
+class Advertisement(models.Model):
+    title = models.CharField('Название', max_length=128)
+    description = models.TextField('Описание')
+    prise = models.DecimalField('Цена', max_digits=10, decimal_places=2)
+    auction = models.BooleanField('Торг', help_text='Отметьте, если торг уместен')
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    @admin.display(description='дата создания')
+    def created_date(self):
+        if self.created_at.date() == timezone.now().date:
+            created_time = self.created_at.time().strftime('%H:%M:%S')
+            return format_html(
+                '<span style="color: green">Сегодня в {} </span>', created_time
+            )
+        return self.created_at.strftime('%d.%m.%Y в %H:%M:%S')
+
+    def __str__(self):
+        return f"Advertisement(id={self.id}, title={self.title}, prise={self.prise})"
+    
+    class Meta:
+        db_table = 'advertisement'
